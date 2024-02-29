bbab085a6a7267d47df6ca60f057dc75fa2a20ac
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2024-02-29 17:01:58 +0100

    group 3 double LED project

--- group-3/src/double-led.py
+++ group-3/src/double-led.py
@@ -3,24 +3,13 @@ from machine import Pin
 from neopixel import NeoPixel
 import time
 
-tickTime = 0.09
+tickTime = 0.12
 
 stripLedCount = 15
 strand = NeoPixel(Pin(2), stripLedCount)
 
 ringLedCount = 12
 ring = NeoPixel(Pin(3), ringLedCount)
-
-# Turn off all LEDs before program start
-ring.fill((0, 0, 0))
-ring.write()
-strand.fill((0, 0, 0))
-strand.write()
-
-strandColor = 255, 0, 0
-ringColor = 0, 255, 0
-
-time.sleep(1)
 
 
 def clear_strand():
@@ -38,10 +27,19 @@ def clear_all():
     clear_ring()
 
 
+# Turn off all LEDs before program start
+clear_all()
+
+time.sleep(1)
+
+big_i = 0
 while True:
+    strandColor = [20, 20, 20]
+    ringColor = [50, 50, 50]
+    strandColor[big_i % 3] = 255
+    ringColor[(big_i + 1) % 3] = 255
 
     for i in range(stripLedCount):
-
         strand[i] = strandColor
         strand.write()
         ring[i % ringLedCount] = ringColor
@@ -52,7 +50,6 @@ while True:
         clear_all()
 
     for i in reversed(range(stripLedCount)):
-
         strand[i] = strandColor
         strand.write()
         ring[i % ringLedCount] = ringColor
@@ -63,3 +60,5 @@ while True:
 
         # Clear the strand at the end of each loop
         clear_all()
+
+    big_i += 1
